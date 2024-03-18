7937791062996ded2689a330b9c757d7f5386deb
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-03-18 19:16:03 -0300

    develope

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import LandingPage from './components/landingpage/LandingPage'
 import Home from './components/home/Home'
 import Detail from './components/detail/Detail'
-/* import ActivityForm from './components/activityform/ActivityForm' */
+import ActivityForm from './components/activityform/ActivityForm'
 import Nav from './components/nav/Nav'
 
 const App = () => {
@@ -16,7 +16,7 @@ const App = () => {
         <Route path='/' element={<LandingPage />} />
         <Route path='/home' element={<Home />} />
         <Route path='/detail/:id' element={<Detail />} />
-{/*         <Route path='/activity' element={<ActivityForm />} /> */}
+        <Route path='/activity' element={<ActivityForm />} />
       </Routes>
     </>
   )
